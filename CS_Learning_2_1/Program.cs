40686fca174ab25fda825ffdf318ff3d1eccf1be
QA Ivan <asconsizq@gmail.com>
2024-09-26 23:13:24 +0500

    add_cs_learning_2

---
+++ CS_Learning_2_1/Program.cs
@@ -0,0 +1,35 @@
+﻿try
+{
+    int[] nums1 = new int[5];
+
+    Console.Write("Введите первое число: ");
+    nums1[0] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите второе число: ");
+    nums1[1] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите третье число: ");
+    nums1[2] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите четвертое число: ");
+    nums1[3] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите пятое число: ");
+    nums1[4] = Convert.ToInt32(Console.ReadLine());
+
+    double num1 = (double)(nums1[0] + nums1[1] + nums1[2] + nums1[3] + nums1[4]) / nums1.Length;
+
+    Console.WriteLine(num1);
+}
+catch (IndexOutOfRangeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Все кончено, в любом случае");
+}
---
+++ CS_Learning_2_2/Program.cs
@@ -0,0 +1,59 @@
+﻿try
+{
+    string?[][] nums = new string[3][];
+
+    nums[0] = new string[2];
+    nums[1] = new string[2];
+    nums[2] = new string[2];
+
+    Console.Write("Введите имя первого пользователя: ");
+    nums[0][0] = Console.ReadLine();
+
+    Console.Write("Введите возраст первого пользователя: ");
+    nums[0][1] = Console.ReadLine();
+    Console.WriteLine("");
+
+    Console.Write("Введите имя второго пользователя: ");
+    nums[1][0] = Console.ReadLine();
+
+    Console.Write("Введите возраст второго пользователя: ");
+    nums[1][1] = Console.ReadLine();
+    Console.WriteLine("");
+
+    Console.Write("Введите имя третьего пользователя: ");
+    nums[2][0] = Console.ReadLine();
+
+    Console.Write("Введите возраст третьего пользователя: ");
+    nums[2][1] = Console.ReadLine();
+    Console.WriteLine("");
+
+    Console.Write("Порядковый номер человека, данные которого необходимо вывести: ");
+    int man = int.Parse(Console.ReadLine());
+
+    if (man == 1)
+    {
+        string m_1_1 = nums[0][0];
+        string m_1_2 = nums[0][1];
+        Console.WriteLine($"Данные первого пользователя - Имя: {m_1_1}, Возраст: {m_1_2}");
+    }
+    else if (man == 2)
+    {
+        string m_2_1 = nums[1][0];
+        string m_2_2 = nums[1][1];
+        Console.WriteLine($"Данные второго пользователя - Имя: {m_2_1}, Возраст: {m_2_2}");
+    }
+    else if (man == 3)
+    {
+        string m_3_1 = nums[2][0];
+        string m_3_2 = nums[2][1];
+        Console.WriteLine($"Данные третьего пользователя - Имя: {m_3_1}, Возраст: {m_3_2}");
+    }
+    else
+    {
+        Console.WriteLine("Пользователя с указанным порядковым номером не существует");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
