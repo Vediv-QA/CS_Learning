035be0ef822c14499265a2609f92d533c171aa45
Vediv <asconsizq@gmail.com>
2024-10-07 00:37:25 +0500

    re1_add_cs_learning_3

--- CS_Learning_3/Program.cs
+++ CS_Learning_3/Program.cs
@@ -1,22 +1,24 @@
 ﻿try
 {
-        int size = 10;
-        var num_1 = new string[size];
-        int i = 0;
-        int err = 0;
-        while (i < num_1.Length)
+    int arrIndex = 0;                   //номер элемента массива
+    int arrErr = 0;                    //счетчик ошибок пользователя
+    string? arrStop = "Q";            //стоп символ
+    double[] arr_1 = new double[5];  //стартовый массив
+
+    while (true)
+    {
+        try
         {
-            Console.Write("Введите число: ");
-            num_1[i] = Console.ReadLine();
-            if (num_1[i] == "Q") // Условия стоп-символа
+            Console.Write("Введите число или стоп-символ Q: ");
+            string? input = Console.ReadLine();
+            if (input.ToUpper().Contains(arrStop))
             {
-                Console.WriteLine("");
-                Console.WriteLine("Был указан стоп-символ, введите порядковый номер необходимого действия: ");
-                Console.WriteLine("1 - Продолжить");
-                Console.WriteLine("2 - Очистить");
-                Console.WriteLine("3 - Выйти ");
-                Console.WriteLine("");
-                Console.Write("Номер: ");
+                Console.WriteLine("\nСозданный массив: ");
+                for (int i = 0; i < arrIndex; i++)
+                {
+                    Console.Write($"{arr_1[i]} ");
+                }
+                Console.Write("\nБыл указан стоп-символ, что делать дальше?:\n1 - Продолжить\n2 - Очистить\n3 - Выйти\nВыбор: ");
                 string? choice = Console.ReadLine();
                 if (choice == "1")
                 {
@@ -24,109 +26,73 @@
                 }
                 else if (choice == "2")
                 {
-                    var num_3 = new string[size];
-                    for (int f = 0; f < num_3.Length; f++)
-                    {
-                        num_1[f] = num_3[f];
-                    }
-                    Console.WriteLine("Массив очищен, введите порядковый номер необходимого действия: " +
-                        "1 - Продолжить " +
-                        "2 - Выйти ");
-                    string? choice1 = Console.ReadLine();
-                    if (choice1 == "1")
+                    arr_1 = new double[5];
+                    arrIndex = 0;
+                    Console.WriteLine("\nМассив очищен^_^");
+                    Console.WriteLine($"Кол-во символов в массиве: {arrIndex}");
+                    Console.WriteLine($"Кол-во ошибок: {arrErr}");
+                    Console.Write("Что делать дальше?:\n1 - Продолжить\n2 - Выйти\nВыбор: ");
+                    string? choice_1 = Console.ReadLine();
+                    if (choice_1 == "1")
                     {
                         continue;
                     }
-                    else if (choice1 == "2")
+                    else if (choice_1 == "2")
                     {
-                        Console.WriteLine($"Кол-во введенных элементов: {i}");
-                        Console.WriteLine($"Кол-во ошибок: {err}");
+                        Console.WriteLine($"Кол-во символов в массиве: {arrIndex}");
+                        Console.WriteLine($"Кол-во ошибок: {arrErr}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Такого варианта нет");
                         break;
                     }
                 }
                 else if (choice == "3")
                 {
-                    Console.WriteLine($"Кол-во введенных элементов: {i}");
-                    Console.WriteLine($"Кол-во ошибок: {err}");
+                    Console.WriteLine($"Кол-во символов в массиве: {arrIndex}");
+                    Console.WriteLine($"Кол-во ошибок: {arrErr}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Такого варианта нет");
+                    arrErr++;
                     break;
                 }
             }
-            else if (num_1[i] != "0" && num_1[i] != "1" && num_1[i] != "2" && num_1[i] != "3" && num_1[i] != "4" && num_1[i] != "5"
-                && num_1[i] != "6" && num_1[i] != "7" && num_1[i] != "8" && num_1[i] != "9") // Ограничения на вовод
+            else
             {
-                Console.WriteLine("Введены некорректные данные! Необходимо ввести число от 1 до 9!");
-                err++; // Счетчик ошибок
-                continue;
-            }
-            i++;
-        }
-        // Выполняется при переполнение массива
-        if (i == num_1.Length)
-        {
-            var num_2 = new string[size + 1000];
-            for (int j = 0; j < num_1.Length; j++)
-            {
-                num_2[j] = num_1[j];
-            }
-            while (i < num_2.Length)
-            {
-                Console.Write("Введите число: ");
-                num_2[i] = Console.ReadLine();
-                if (num_2[i] == "Q") // Условия стоп-символа
+                var arrSymbol = double.Parse(input);
+                if (arrIndex < arr_1.Length)
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("Был указан стоп-символ, введите порядковый номер необходимого действия: ");
-                    Console.WriteLine("1 - Продолжить");
-                    Console.WriteLine("2 - Очистить");
-                    Console.WriteLine("3 - Выйти ");
-                    Console.WriteLine("");
-                    Console.Write("Номер: ");
-                    string? choice = Console.ReadLine();
-                    if (choice == "1")
+                    arr_1[arrIndex] = arrSymbol;
+                    double[] arr_2 = new double[arr_1.Length * 2];
+                    for (int i = 0; i < arr_1.Length; i++)
                     {
-                        continue;
+                        arr_2[i] = arr_1[i];
                     }
-                    else if (choice == "2")
-                    {
-                        var num_4 = new string[size + 1000];
-                        for (int k = 0; k < num_4.Length; k++)
-                        {
-                            num_2[k] = num_4[k];
-                        }
-                        Console.WriteLine("Массив очищен, введите порядковый номер необходимого действия: " +
-                            "1 - Продолжить " +
-                            "2 - Выйти ");
-                        string? choice1 = Console.ReadLine();
-                        if (choice1 == "1")
-                        {
-                            continue;
-                        }
-                        else if (choice1 == "2")
-                        {
-                            Console.WriteLine($"Кол-во введенных элементов: {i}");
-                            Console.WriteLine($"Кол-во ошибок: {err}");
-                            break;
-                        }
-                    }
-                    else if (choice == "3")
-                    {
-                        Console.WriteLine($"Кол-во введенных элементов: {i}");
-                        Console.WriteLine($"Кол-во ошибок: {err}");
-                        break;
-                    }
+                    arr_1 = arr_2;
+                    arrIndex++;
                 }
-                else if (num_2[i] != "0" && num_2[i] != "1" && num_2[i] != "2" && num_2[i] != "3" && num_2[i] != "4" && num_2[i] != "5"
-                && num_2[i] != "6" && num_2[i] != "7" && num_2[i] != "8" && num_2[i] != "9") // Ограничения на вовод
-                {
-                    Console.WriteLine("Введены некорректные данные! Необходимо ввести число от 1 до 9!");
-                    err++; // Счетчик ошибок
-                    continue;
-                }
-                i++;
             }
-        } 
-}
 
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("\nВведен некорректный символ!");
+            Console.WriteLine("Созданный массив: ");
+            for (int i = 0; i < arrIndex; i++)
+            {
+                Console.Write($"{arr_1[i]} ");
+            }
+            Console.WriteLine("");
+            arrErr++;
+            continue;
+        }
+    }
+}
 catch (Exception ex)
 {
     Console.WriteLine($"что-то пошло не так: {ex.Message}");
