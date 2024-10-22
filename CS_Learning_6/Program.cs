43da594c60702cc608a25fe4863dd1a2f3f12558
Vediv <asconsizq@gmail.com>
2024-10-22 10:18:12 +0500

    add_cs_learning_6-7

---
+++ CS_Learning_6/Program.cs
@@ -0,0 +1,252 @@
+﻿//по ДЗ 6:
+//использовать enum в игре с курицей
+//В игре с курицей сделать теперь 3 курицы
+
+Console.WriteLine("");
+Console.WriteLine("Помогите трём курочкам выполнить их священный долг - заберите у каждой курочки 10 снесенных яиц!");
+Console.WriteLine("Чтобы выбранная курочка снесла яйцо, кормите ее каждый ход 3-5 зернышками!");
+Console.WriteLine("Если курочку не покормить один ход, то она умирает!");
+int atosEgg = 0;
+int partosEgg = 0;
+int aramisEgg = 0;
+int player_atosEgg = 0;
+int player_partosEgg = 0;
+int player_aramisEgg = 0;
+bool chikenLive_1 = true;
+bool chikenLive_2 = true;
+bool chikenLive_3 = true;
+ChickenName chikenName = ChickenName.Atos;
+while (chikenLive_1 | chikenLive_2 | chikenLive_3)
+{
+    try
+    {
+        Console.WriteLine("");
+        Console.Write("Выберите курочку:\n1 - Атос\n2 - Партос\n3 - Арамис\nВыбор: ");
+        int chiken = Convert.ToInt32(Console.ReadLine());
+        if (chiken == 1)
+        {
+            chikenName = ChickenName.Atos;
+            Console.WriteLine("");
+            Console.WriteLine("Вы выбрали курочку по имени Атос");
+            if (chikenLive_1 == false)
+            {
+                Console.WriteLine("Курочка Атос уже умерла, выберите другую курочку!");
+                continue;
+            }
+        }
+        if (chiken == 2)
+        {
+            chikenName = ChickenName.Partos;
+            Console.WriteLine("");
+            Console.WriteLine("Вы выбрали курочку по имени Партос");
+            if (chikenLive_2 == false)
+            {
+                Console.WriteLine("Курочка Партос уже умерла, выберите другую курочку!");
+                continue;
+            }
+        }
+        if (chiken == 3)
+        {
+            chikenName = ChickenName.Aramis;
+            Console.WriteLine("");
+            Console.WriteLine("Вы выбрали курочку по имени Арамис");
+            if (chikenLive_3 == false)
+            {
+                Console.WriteLine("Курочка Арамис уже умерла, выберите другую курочку!");
+                continue;
+            }
+        }
+        if (chiken != 1 && chiken != 2 && chiken != 3)
+        {
+            Console.WriteLine("Такого варианта нет, корректно выберите действие!");
+            continue;
+        }
+
+        while (true)
+        { 
+            if (chikenName == ChickenName.Atos)
+            {
+                Console.Write("Выберите действие:\n1 - Покормить курочку\n2 - Забрать яйца\n3 - Ничего не делать\nВыбор: ");
+                chooseAction();
+                if (atosEgg == 10)
+                {
+                    collectEgg();
+                    break;
+                }
+                else if (chikenLive_1 == false)
+                {
+                    break;
+                }
+            }
+            if (chikenName == ChickenName.Partos)
+            {
+                Console.Write("Выберите действие:\n1 - Покормить курочку\n2 - Забрать яйца\n3 - Ничего не делать\nВыбор: ");
+                chooseAction();
+                if (partosEgg == 10)
+                {
+                    collectEgg();
+                    break;
+                }
+                else if (chikenLive_2 == false)
+                {
+                    break;
+                }
+            }
+            if (chikenName == ChickenName.Aramis)
+            {
+                Console.Write("Выберите действие:\n1 - Покормить курочку\n2 - Забрать яйца\n3 - Ничего не делать\nВыбор: ");
+                chooseAction();
+                if (aramisEgg == 10)
+                {
+                    collectEgg();
+                    break;
+                }
+                else if (chikenLive_3 == false)
+                {
+                    break;
+                }
+            }
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Такого варианта нет, корректно выберите действие!");
+        continue;
+    }
+}
+void chooseAction()
+{
+    int action = Convert.ToInt32(Console.ReadLine());
+    switch (action)
+    {
+        case 1:
+            bringFood();
+            break;
+        case 2:
+            collectEgg();
+            break;
+        case 3:
+            doNothing();
+            break;
+        default:
+            Console.WriteLine("Такого варианта нет, корректно выберите действие!");
+            break;
+    }
+}
+void bringFood()
+{
+    Console.WriteLine("Кормим курочку зерном! Укажите кол-во зернышек: ");
+    int corn = Convert.ToInt32(Console.ReadLine());
+    if (corn >= 3 && corn <= 5)
+    {
+        if (chikenName == ChickenName.Atos)
+        {
+            atosEgg++;
+            Console.WriteLine($"\nКурочка Атос снесла {atosEgg}-е яйцо!");
+        }
+        if (chikenName == ChickenName.Partos)
+        {
+            partosEgg++;
+            Console.WriteLine($"\nКурочка Партос снесла {partosEgg}-е яйцо!");
+        }
+        if (chikenName == ChickenName.Aramis)
+        {
+            aramisEgg++;
+            Console.WriteLine($"\nКурочка Партос снесла {aramisEgg}-е яйцо!");
+        }
+    }
+    else if (corn > 5)
+    {
+        Console.WriteLine("Курочка переела! Слишком много зерен!");
+        if (chikenName == ChickenName.Atos)
+        {
+            chikenLive_1 = false;
+        }
+        if (chikenName == ChickenName.Partos)
+        {
+            chikenLive_2 = false;
+        }
+        if (chikenName == ChickenName.Aramis)
+        {
+            chikenLive_3 = false;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Курочка недоела! Слишком мало зерен!");
+        if (chikenName == ChickenName.Atos)
+        {
+            Console.WriteLine("Курочка Атос погибла!");
+            chikenLive_1 = false;
+        }
+        if (chikenName == ChickenName.Partos)
+        {
+            Console.WriteLine("Курочка Партос погибла!");
+            chikenLive_2 = false;
+        }
+        if (chikenName == ChickenName.Aramis)
+        {
+            Console.WriteLine("Курочка Арамис погибла!");
+            chikenLive_3 = false;
+        }
+    }
+}
+void collectEgg()
+{
+    Console.WriteLine("Забираем у курочки яйца");
+    if (chikenName == ChickenName.Atos)
+    {
+        Console.WriteLine("Курочка Атос погибла");
+        player_atosEgg = atosEgg;
+        chikenLive_1 = false;
+    }
+    if (chikenName == ChickenName.Partos)
+    {
+        Console.WriteLine("Курочка Партос погибла");
+        player_partosEgg = partosEgg;
+        chikenLive_2 = false;
+    }
+    if (chikenName == ChickenName.Aramis)
+    {
+        Console.WriteLine("Курочка Партос погибла");
+        player_aramisEgg = aramisEgg;
+        chikenLive_3 = false;
+    }
+}
+void doNothing()
+{
+    Console.WriteLine("Ничего не делаем");
+    if (chikenName == ChickenName.Atos)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Курочка Атос погибла!");
+        chikenLive_1 = false;
+    }
+    if (chikenName == ChickenName.Partos)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Курочка Партос погибла!");
+        chikenLive_2 = false;
+    }
+    if (chikenName == ChickenName.Aramis)
+    {
+        Console.WriteLine("");
+        Console.WriteLine("Курочка Арамис погибла!");
+        chikenLive_3 = false;
+    }
+}
+if (player_atosEgg == 10 && player_partosEgg == 10 && player_aramisEgg == 10)
+{
+    Console.WriteLine($"\nИгра успешно завершена, собранное кол-во яиц:\nАтос - {player_atosEgg},\nПартос - {player_partosEgg},\nАрамис - {player_aramisEgg}\nКурочки выполнили свой долг! Один за всех и все за одного! Мы будем помнить их!!!");
+}
+else
+{
+    Console.WriteLine($"\nИгра провалена, собранное кол-во яиц:\nАтос - {player_atosEgg},\nПартос - {player_partosEgg},\nАрамис - {player_aramisEgg}\nОдна из курочек погибла за всех и все за одну, они не успели выполнить свой долг, вы плохой фермер!");
+}
+enum ChickenName
+{
+    Atos,
+    Partos,
+    Aramis
+}
+
---
+++ CS_Learning_7/Program.cs
@@ -0,0 +1,87 @@
+﻿//ДЗ 7
+//1. Записать что-то в файл, прочитать что-то из файла
+//можно сделать что-то вроде простого блокнота, 
+//который записывает в файл всё то что пользователь туда записывает
+//2. Расширяем функционал прошлого задания - возможность сохранять в разные файлы 
+//(спрашивать у пользователя куда сохранить)
+//3. Предлагать пользователю открыть дописать в какой-то в файл 
+//(допустим все заметки сохраняются в одну папку и выводить пользователю 
+//список файлов и просить выбрать какой открыть)
+
+string? choice = "";
+while (choice != "4")
+{
+    Console.WriteLine("Выберите действие:\n1 - Создать новый файл\n2 - Добавить запись в существующий файл\n3 - Прочитать информацию из файла\n4 - Закрыть программу");
+    choice = Console.ReadLine();
+    {
+        switch (choice)
+        {
+            case "1":
+                createFile();
+                break;
+            case "2":
+                AddInfo();
+                break;
+            case "3":
+                ReadFile();
+                break;
+            case "4":
+                Console.WriteLine("Закрытие программы");
+                break;
+            default:
+                Console.WriteLine("Такого варианта нет, корректно выберите действие!\n");
+                break;
+        }
+    }
+}
+void createFile()
+{
+    Console.Write("Введите имя создаваемого файла: ");
+    string? newfile = Console.ReadLine() + ".txt";
+    if (!File.Exists(newfile))
+    {
+        Console.Write("Введите текст файла: ");
+        File.WriteAllText(newfile, $"{Environment.NewLine}{Console.ReadLine()}");
+    }
+    else
+    {
+        Console.WriteLine("Файл с указанным именем уже существует!\n");
+    }
+}
+void AddInfo()
+{
+    Console.Write("Введите имя файла, в который необходимо добавить информацию: ");
+    string? file = Console.ReadLine() + ".txt";
+    if (File.Exists(file))
+    {
+        Console.Write("Введите добавляемый в файл текст: ");
+        File.AppendAllText(file, $"{Environment.NewLine}{Console.ReadLine()}");
+    }
+    else
+    {
+        Console.WriteLine("Не существует файла с указанным именем!\n");
+    }
+}
+void ReadFile()
+{
+    ShowFiles();
+    Console.Write("\nУкажите имя файла из списка, информацию из которого необходимо прочитать: ");
+    string? file = Console.ReadLine() + ".txt";
+    if (File.Exists(file))
+    {
+        var info = File.ReadAllText(file);
+        Console.WriteLine(info);
+    }
+    else
+    {
+        Console.WriteLine("Не существует файла с указанным именем!\n");
+    }
+}
+void ShowFiles()
+{
+    string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+    foreach (string file in files)
+    {
+        Console.WriteLine(file);   
+    }
+}
